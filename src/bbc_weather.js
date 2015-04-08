6bf1e1ec82e4efe43c9a78793d027836c8802f62
Eleni Mikroyannidi <mikrohelen@gmail.com>
2015-04-08 09:30:53 +0100

    First commit
    Basic functionality; 
    1) Displays Time and Date
    2) Displays BBC weather based on your location
    3) Displays icon depending on basic weather patterns

---
+++ src/bbc_weather.js
@@ -0,0 +1,128 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function (e) {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+
+function locationSuccess(pos, id) {
+  // Construct URL
+  //var url = "http://open.live.bbc.co.uk/weather/feeds/en/2643123/observations.rss"; 
+  console.log("latitude" + pos.coords.latitude + "and lon=" + pos.coords.longitude);
+  
+  //get geoname id and location name by calling ge
+  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
+      pos.coords.latitude + "&lon=" + pos.coords.longitude;
+  console.log(url);
+  
+  xhrRequest(url, 'GET',
+    function(responseText){
+      var json = JSON.parse(responseText);
+      var location_name = json.name;
+      var geoname_id = json.id;
+      console.log(location_name);
+      console.log(geoname_id);
+      
+      var bbc_url = "http://open.live.bbc.co.uk/weather/feeds/en/" + geoname_id + "/observations.rss";
+      console.log(bbc_url);
+      
+      // Send request to OpenWeatherMap
+      xhrRequest(bbc_url, 'GET', 
+        function(responseText) {
+          // responseText contains an XML object with weather info
+          console.log(responseText);
+          var item = responseText.substring(responseText.indexOf('<item>'), responseText.indexOf('</item>'));
+          var description = item.substring(item.indexOf('<description>'), item.indexOf('</description>'));
+          var temperature = description.substring(description.indexOf("Temperature: ")+13, description.indexOf("C (")+1);
+          var conditions = item.substring(item.indexOf("BST: ")+5, item.indexOf(", ")); 
+          var imageid = 6;
+          
+          console.log(imageid); 
+          console.log(conditions);
+          if(conditions.toLowerCase().indexOf("light cloud") != -1){
+             imageid = 5;
+          }
+          else if(conditions.toLowerCase().indexOf("cloudy") != -1){
+            imageid = 4;
+          }
+          else if(conditions.toLowerCase().indexOf("sunny intervals") != -1){
+            imageid = 6;
+          }
+          else if (conditions.toLowerCase().indexOf("sunny") != -1){
+            imageid = 3;
+          }
+          else if (conditions.toLowerCase().indexOf("clear sky") != -1){
+            imageid = 7;
+          }
+          else if (conditions.toLowerCase().indexOf("partly cloudy") != -1){
+            imageid = 8;
+          }
+          
+          
+          console.log(imageid);  
+          console.log(location_name);
+         //var json = JSON.parse(responseText);
+        
+          // Assemble dictionary using our keys
+          var dictionary = {
+            "KEY_TEMPERATURE": temperature,
+            "KEY_CONDITIONS": conditions,
+            "KEY_IMAGE": imageid,
+            "LOCATION_NAME": location_name
+          };
+          
+          
+          console.log(dictionary);
+    
+          // Send to Pebble
+          Pebble.sendAppMessage(dictionary,
+            function(e) {
+              console.log("Weather info sent to Pebble successfully!");
+            },
+            function(e) {
+              console.log("Error sending weather info to Pebble!");
+            }
+          );
+        }      
+      );
+      
+    });
+  
+  
+}
+
+function locationError(err) {
+  console.log("Error requesting location!");
+  console.log(err);
+}
+
+function getWeather() {
+  console.log("inside getWeather..");
+  var id = navigator.geolocation.getCurrentPosition(locationSuccess, locationError, {enableHighAccuracy: true, 
+     maximumAge: 0, 
+     timeout: 5000});
+}
+
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    getWeather();
+    // Get the initial weather
+    //locationSuccess();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    //Get current location 
+    getWeather();
+  }                     
+);
---
+++ src/main.c
@@ -0,0 +1,251 @@
+#include <pebble.h>
+
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+#define KEY_IMAGE 2
+#define SUNNY 3
+#define CLOUDY 4
+#define LIGHT_CLOUD 5
+#define SUNNY_INTERVALS 6
+#define IMAGE_CLEAR 7
+#define IMAGE_PARTLY_CLOUDY 8
+#define LOCATION_NAME 20
+  
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+static BitmapLayer *s_weather_layer;
+static GBitmap *s_background_bitmap;
+static TextLayer *s_weather_text_layer;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+  static char date_buffer[] = "Mon 11 Jan";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    //Use 2h hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    //Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+  
+  strftime(date_buffer, sizeof("Mon 11 Jan"), "%a %e %b", tick_time);
+  
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_date_layer, date_buffer);
+  
+}
+
+static void set_weather_icon(int resource_id){
+  s_background_bitmap = gbitmap_create_with_resource(resource_id);
+  bitmap_layer_set_bitmap(s_weather_layer, s_background_bitmap);
+}
+
+static void update_weather_image(int weather_image){
+   if (weather_image == SUNNY){
+        set_weather_icon(RESOURCE_ID_IMAGE_WEATHER_SUNNY);
+      }
+      else if(weather_image == LIGHT_CLOUD){
+        set_weather_icon(RESOURCE_ID_IMAGE_WEATHER_LIGHT_CLOUD);
+      }
+      else if( weather_image == CLOUDY){
+        set_weather_icon(RESOURCE_ID_IMAGE_CLOUDY);
+      }
+      else if(weather_image == SUNNY_INTERVALS){
+        set_weather_icon(RESOURCE_ID_IMAGE_SUNNY_INTERVALS);
+      }
+      else if(weather_image == IMAGE_CLEAR){
+        set_weather_icon(RESOURCE_ID_IMAGE_CLEAR);
+      }
+      else if(weather_image == IMAGE_PARTLY_CLOUDY){
+        set_weather_icon(RESOURCE_ID_IMAGE_PARTLY_CLOUDY);
+      }
+      else{
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Weather Key %d not recognized!", (int)weather_image);
+      }
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+  
+  // Get weather update every 60 minutes
+  if(tick_time->tm_min % 60 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+  
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+  
+    // Send the message!
+    app_message_outbox_send();
+  }
+  
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[32];
+  static char conditions_buffer[32];
+  static char location_buffer[32];
+  static char weather_layer_buffer[32];
+  static int weather_image;
+  
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_TEMPERATURE:
+      snprintf(temperature_buffer, sizeof(temperature_buffer), "%s", t->value->cstring);
+      break;
+    case KEY_CONDITIONS:
+      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+      //update_weather_image();
+      break;
+    case LOCATION_NAME:
+      snprintf(location_buffer, sizeof(location_buffer), "%s", t->value->cstring);
+    case KEY_IMAGE:
+      weather_image = (int)t->value->int32;
+      update_weather_image(weather_image);
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  
+  // Assemble full string and display
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s, %s", temperature_buffer, conditions_buffer, location_buffer);
+  text_layer_set_text(s_weather_text_layer, weather_layer_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void main_window_load(Window *window) {
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 0, 144, 43));
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+   // Create date TextLayer
+  s_date_layer = text_layer_create(GRect(0, 43, 144, 19));
+  text_layer_set_background_color(s_date_layer, GColorBlack);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text(s_date_layer, "Mon 1 Jan");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  
+  
+  // Create weather image Layer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BBC);
+  s_weather_layer = bitmap_layer_create(GRect(0, 63, 75, 60));
+  bitmap_layer_set_bitmap(s_weather_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_weather_layer));
+  
+  //Create weather text Layer
+  s_weather_text_layer = text_layer_create(GRect(0, 115, 144, 80));
+  text_layer_set_background_color(s_weather_text_layer, GColorClear);
+  text_layer_set_text_color(s_weather_text_layer, GColorBlack);
+  text_layer_set_text_alignment(s_weather_text_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_text_layer, "Loading BBC Weather...");
+  
+  // Add it as a child layer to the Window's root layer
+  text_layer_set_font(s_weather_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_text_layer));
+  
+  // Make sure the time is displayed from the start
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  
+  // Destroy TextLayer
+  text_layer_destroy(s_date_layer);
+  
+  //Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+
+  //Destroy BitmapLayer
+  bitmap_layer_destroy(s_weather_layer);
+  
+  // Destroy weather elements
+  text_layer_destroy(s_weather_text_layer);
+}
+
+
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+  
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
